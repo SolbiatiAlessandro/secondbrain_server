28cbb1eb0058a7e8182667969a45258e167043a4
Alessandro Solbiati <lessandro@fb.com>
2022-11-01 00:25:24 -0700

    [logging] datadog logging

---
+++ bin/log.sh
@@ -0,0 +1,17 @@
+source datadog-secret.sh
+NODEID="${1}"
+NODETITLE="${2}"
+ACTION="${3}"
+curl -X POST "https://http-intake.logs.datadoghq.com/api/v2/logs" \
+-H "Accept: application/json" \
+-H "Content-Type: application/json" \
+-H "DD-API-KEY: $APIKEY" \
+-d @- << EOF
+[
+  {
+    "nodeid": "$NODEID",
+    "nodetitle": "$NODETITLE",
+    "action": "$ACTION",
+  }
+]
+EOF
